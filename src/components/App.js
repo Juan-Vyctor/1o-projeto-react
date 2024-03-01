ba3f342b93ed7f298bc244a224552426cddadca9
Juan <juan.vyctor@escolar.ifrn.edu.br>
2024-03-01 08:00:01 -0300

    1a aula

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
 import logo from '../logo.svg';
 import './css/App.css';
-
-// faz o import do coiso default do component
 import Teste from './Teste.js';
-
-// quando tem {} o import e de uma função sem ser a default
-import {Texto} from './Teste.js';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				{/* dá pra puxar outras página dentro de uma página e vai encadeando */}
-				<Teste />
 				<img src={logo} className="App-logo" alt="logo" />
+				<Teste />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
@@ -25,7 +19,8 @@ function App() {
 				>
 					Learn React
 				</a>
-				<Texto />
+				<Teste />
+				{/* dá pra puxar outras página dentro de uma página e vai encadeando */}
 			</header>
 		</div>
 	);
--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,20 +1,9 @@
 import './css/Teste.css';
 
-// função que dá pra chamar por fora
-export function Texto() {
-	return (
-		<div>
-			<h1>Isaura eh bonita</h1>
-		</div>
-	)
-}
-
-// a principal que vai voltar se chamar seco
 function Teste() {
 	return (
 		<div className="Teste">
-			<h1>aaaaaaa</h1>
-			<h1>Isaura eh feia</h1>
+		<h1>aaaaaaa</h1>
 		</div>
 	);
 }
