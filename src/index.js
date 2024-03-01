9a8e3d81f241a3b9856d046eafd86d6c2a44edc4
Juan <juan.vyctor@escolar.ifrn.edu.br>
2024-03-01 08:04:11 -0300

    1o desafio

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
+import App from './components/App.js';
+
+import Profile from './desafios/export.js';
 
 // essa const que faz rodar de verdade tipo root mesmo pa
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		{/* <App /> */}
+		<Profile />
 	</React.StrictMode>
 );
 
