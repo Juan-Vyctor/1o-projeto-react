d67e69d4e3e7412214ee374e8c174f474bc05788
Juan <juan.vyctor@escolar.ifrn.edu.br>
2024-03-22 08:01:33 -0300

    base feita

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,18 +11,35 @@ function Ordem(lista) {
     )
 }
 
+function Estrutura(titulo, lista) {
+    return(
+        <li className="dropdown">
+            <a href="#">
+                <div className="dropdown-menu">
+                    {titulo}
+                    {Ordem(lista)}
+                </div>
+            </a>
+        </li>
+    )
+}
+
 export default function Dropdown (props) {
     return (
         <div>
             <ul>
                 <li className="dropdown">
-                    <a href="">
+                    <a href="#">
                         <div className="dropdown-menu">
                             {props.titulo[0]}
                             {Ordem(props.lista)}
                         </div>
                     </a>
                 </li>
+                {Estrutura(props.titulo[1], props.lista)}
+                {Estrutura(props.titulo[2], props.lista)}
+                {Estrutura(props.titulo[3], props.lista)}
+                {Estrutura(props.titulo[4], props.lista)}
             </ul>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App.js';
+import Nav from './components/Nav.js';
+import Dropdown from './components/Dropdown.js';
 import reportWebVitals from './reportWebVitals';
 import homura from './homura.gif'
+
+const titulo = ['a', 'b', 'c', 'd', 'e'];
+const nomes = ['menu 1', 'menu 2', 'menu 3'];
 
 // essa const que faz rodar de verdade tipo root mesmo pa
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 	{/* <Gallery /> */}
+	<Nav>
+		<Dropdown titulo={titulo} lista={nomes}/>
+	</Nav>
 	<App nome="patrocinado por L'Oreal Paris" personagem={homura} sla='https://64.media.tumblr.com/b2c5e2a38641735f31b4106b0b363139/49040cb68629585b-91/s1280x1920/bca05f8dda44af8b3735eebf5350756d2c5726eb.gifv'/>
 	</React.StrictMode>
 );
