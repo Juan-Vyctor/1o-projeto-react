179c2767bc64aa0abfb65af885f108adf18e7a6d
Juan Vyctor Silva Garcia de Oliveira <juan.vyctor@escolar.ifrn.edu.br>
2024-08-21 10:17:38 -0300

    comeco das options

--- src/components/Hook.js
+++ src/components/Hook.js
@@ -4,11 +4,12 @@ export default function Hook() {
     function cadastrarUsuario(e) {
         // o preventDefault() faz que ignore que faz normalmente
         e.preventDefault();
-        alert(`o nome passado foi ${name}`)
+        alert(`o nome passado foi ${name}`);
     }
 
     // isso aqui é o Hook, o useState() é uma memória pro front e vai ficar salva mesmo sem ir pro back
     const [name,setName] = useState();
+    const [selectedOption,setSelectedOption] = useState();
 
     return (
         <div>
@@ -25,6 +26,12 @@ export default function Hook() {
                 </label>
                 <input type="submit" />
             </form>
+            <br />
+            <select value={selectedOption} onChange={handleSelectChange}>
+                <option>Selecione uma opção</option>
+                <option value='1'>um</option>
+                <option value='2'>dois</option>
+            </select>
         </div>
     )
 }
